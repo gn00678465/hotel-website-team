abbb57d06822cdbb28f7c98506c02f58e5d70d6c
Madao <gn00678465@gmail.com>
2024-01-12 00:15:14 +0800

    feat: 建立 DatePicker Mobile 檔案

--- src/components/DatePicker/DatePicker.vue
+++ src/components/DatePicker/DatePicker.vue
@@ -1,5 +1,9 @@
 <template>
-  <div ref="datePickerRef" class="flex w-fit items-center gap-x-2" @click="showModal = true">
+  <div
+    ref="datePickerRef"
+    class="hidden w-fit items-center gap-x-2 desktop:flex"
+    @click="showModal = true"
+  >
     <date-input label="入住" :date="formatDate(dates?.[0])"></date-input>
     <date-input label="退房" :date="formatDate(dates?.[1])"></date-input>
   </div>
@@ -21,13 +25,17 @@ import { useBoolean, useRect } from '@/hooks';
 import { curryFormatDate } from './utils';
 
 const datePickerRef = ref<null | HTMLDivElement>(null);
-const { bool: showModal } = useBoolean(false);
+const { bool: showModal, setTrue: setModalShow } = useBoolean(false);
 const position = reactive({ top: 0, right: 0 });
 const datePickerRect = useRect(datePickerRef);
 const dates = ref<[number, number] | null>(null);
 
 function formatDate(date?: number): string {
   return (date && curryFormatDate('YYYY/ MM / DD')(date)) || '-';
+}
+
+function onModalShow() {
+  setModalShow();
 }
 
 watch(showModal, (show) => {
@@ -37,6 +45,10 @@ watch(showModal, (show) => {
       position.right = window.innerWidth - datePickerRect.value.right - 32;
     }
   }
+});
+
+defineExpose({
+  onModalShow,
 });
 </script>
 
