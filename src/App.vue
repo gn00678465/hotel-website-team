eeb473cc3b99bf690a2f031486df2d9f52b2587f
Madao <gn00678465@gmail.com>
2024-01-14 17:12:43 +0800

    fix(app.vue): 修正 App.vue 結構

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,12 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router';
-import BaseNav from '@/components/layout/BaseNav.vue';
 import BaseFooter from '@/components/layout/BaseFooter.vue';
 import BaseModal from '@/components/BaseModal.vue';
 </script>
 
 <template>
-  <div>
-    <BaseNav />
-
-    <main class="p-10">
-      <RouterView />
-    </main>
-
-    <BaseFooter />
-  </div>
+  <RouterView />
+  <BaseFooter />
 </template>
 
 <style></style>
