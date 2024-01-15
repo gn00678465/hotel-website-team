920f1f08d5b3993c01e284d1ee834b3d89b76b69
Madao <gn00678465@gmail.com>
2024-01-15 20:12:30 +0800

    fix(component): 調整 HeroButton 樣式

--- src/components/HeroButton.vue
+++ src/components/HeroButton.vue
@@ -1,38 +1,35 @@
 <template>
-	<div
-		class="group flex justify-center items-center rounded-lg p-10 cursor-pointer text-neutral-black bg-neutral-white duration-300 hover:bg-primary-100 hover:text-neutral-white hover:justify-end"
+  <div
+    class="group flex cursor-pointer items-center justify-center rounded-lg bg-neutral-white p-5 text-neutral-black duration-300 hover:justify-end hover:bg-primary-100 hover:text-neutral-white desktop:p-10"
   >
-    <div class="flex w-full">
-			<div class="flex-1 w-3/5"></div>
-			<div class="flex items-center w-2/5 duration-300 group-hover:justify-end">
-				<div class="pr-4 text-h5">
-					{{ label }}
-				</div>
-				<div class="h-[1px] w-full bg-neutral-black duration-300 group-hover:bg-neutral-white group-hover:w-3/5"></div>
-			</div>
-		</div>
+    <div class="flex w-full items-center justify-end">
+      <div
+        class="pr-4 text-body leading-heading tracking-heading desktop:text-h5 desktop:leading-normal desktop:tracking-normal"
+      >
+        <slot>{{ label }}</slot>
+      </div>
+      <div
+        class="h-[1px] w-20 bg-neutral-black duration-300 group-hover:w-3/5 group-hover:bg-neutral-white desktop:w-[150px] desktop:group-hover:w-[120px]"
+      ></div>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-
 const props = defineProps({
-	buttonType: {
-		type: String,
+  buttonType: {
+    type: String,
     default: 'primary',
-	},
-	isDisable: {
-		type: Boolean,
+  },
+  isDisable: {
+    type: Boolean,
     default: false,
-	},
-	label: {
+  },
+  label: {
     type: String,
     default: 'label',
   },
 });
-
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
