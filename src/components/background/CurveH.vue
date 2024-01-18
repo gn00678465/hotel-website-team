efc3b27059c17c5525f75aa0604be64f02414e06
Madao <gn00678465@gmail.com>
2024-01-18 09:05:10 +0800

    feat(component): 調整 curve svg component

--- src/components/background/CurveH.vue
+++ src/components/background/CurveH.vue
@@ -1,52 +1,42 @@
 <template>
   <div>
-    <div class="curve-h relative">
-      <svg
-        v-for="n in count"
-        :key="n"
-        xmlns="http://www.w3.org/2000/svg"
-        :width="width"
-        :height="height"
-        viewBox="0 0 1060 80"
-        fill="none"
-        class="absolute"
-        :style="{ top: `calc(${n} * var(--space))` }"
-      >
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="100%"
+      height="100%"
+      :viewBox="`0 0 1920 ${80 + count * space}`"
+      fill="none"
+    >
+      <defs>
         <path
-          d="M1218.15 74.8338C1174.15 80.4951 1056.35 85.2301 937.097 58.8791C788.031 25.9405 659.928 -13.1742 447.975 4.32449C236.022 21.8232 157.607 43.4391 0.000366211 23.8818"
+          id="curve-h"
+          d="M1920 74.8338C1850.65 80.4951 1664.98 85.2301 1477.02 58.8791C1242.06 25.9405 1040.15 -13.1742 706.079 4.32449C372.006 21.8232 248.411 43.4391 -0.0019368 23.8818"
           :stroke="color"
         />
-      </svg>
-    </div>
+      </defs>
+      <use v-for="n of count" :key="n" :y="0 + n * space" xlink:href="#curve-h" />
+    </svg>
   </div>
 </template>
 
 <script setup lang="ts">
-import { toRefs } from 'vue';
+import { toRefs, computed } from 'vue';
+import { useMediaQuery } from '@vueuse/core';
 
 interface Props {
   color: string;
-  width: number;
-  height: number;
   count: number;
 }
 
 const props = withDefaults(defineProps<Partial<Props>>(), {
   count: 1,
   color: '#BF9D7D',
-  width: 1060,
-  height: 80,
 });
 
-const { color, width, height, count } = toRefs(props);
+const isMobile = useMediaQuery('(max-width: 768px)');
+const { color, count } = toRefs(props);
+
+const space = computed(() => (isMobile.value ? 7.48 : 12));
 </script>
 
-<style scoped lang="scss">
-.curve-h {
-  @apply relative;
-  --space: 7.48px;
-  @media (min-width: 768px) {
-    --space: 12px;
-  }
-}
-</style>
+<style scoped></style>
--- src/components/background/CurveV.vue
+++ src/components/background/CurveV.vue
@@ -1,52 +1,40 @@
 <template>
-  <div>
-    <div class="curve-v relative">
-      <svg
-        v-for="n in count"
-        :key="n"
-        xmlns="http://www.w3.org/2000/svg"
-        :width="width"
-        :height="height"
-        viewBox="0 0 80 1068"
-        fill="none"
-        class="absolute"
-        :style="{ left: `calc(${n} * var(--space))` }"
-      >
-        <path
-          d="M5.16621 1218.15C-0.495123 1174.15 -5.23006 1056.35 21.1209 937.097C54.0595 788.031 93.1742 659.927 75.6755 447.974C58.1769 236.021 36.561 157.606 56.1183 -0.000123114"
-          :stroke="color"
-        />
-      </svg>
-    </div>
-  </div>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="100%"
+    height="100%"
+    viewBox="0 0 80 1068"
+    fill="none"
+  >
+    <defs>
+      <path
+        id="curve-v"
+        d="M5.16621 1218.15C-0.495123 1174.15 -5.23006 1056.35 21.1209 937.097C54.0595 788.031 93.1742 659.927 75.6755 447.974C58.1769 236.021 36.561 157.606 56.1183 -0.000123114"
+        :stroke="color"
+      />
+    </defs>
+    <use v-for="n of count" :key="n" :x="0 + n * space" xlink:href="#curve-v" />
+  </svg>
 </template>
 
 <script setup lang="ts">
-import { toRefs } from 'vue';
+import { toRefs, computed } from 'vue';
+import { useMediaQuery } from '@vueuse/core';
 
 interface Props {
   color: string;
-  width: number;
-  height: number;
   count: number;
 }
 
 const props = withDefaults(defineProps<Partial<Props>>(), {
   count: 1,
   color: '#BF9D7D',
-  width: 80,
-  height: 1068,
 });
 
-const { color, width, height, count } = toRefs(props);
+const isMobile = useMediaQuery('(max-width: 768px)');
+const { color, count } = toRefs(props);
+
+const space = computed(() => (isMobile.value ? 7.48 : 12));
 </script>
 
-<style scoped>
-.curve-v {
-  @apply relative;
-  --space: 7.48px;
-  @media (min-width: 768px) {
-    --space: 12px;
-  }
-}
-</style>
+<style scoped></style>
